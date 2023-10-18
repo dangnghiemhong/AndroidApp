3cb1e98e5e695d99314c99477f3f8c601f120443
dangnghiemhong <dangnghiem200@gmail.com>
2023-10-18 19:35:52 +0700

    Update

--- app/src/main/java/com/example/rolldice/MainActivity.kt
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.rolldice
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -26,8 +27,21 @@ class MainActivity : ComponentActivity() {
         val diceRoll = dice.roll()
 
         // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 class Dice(private val numSides: Int) {
